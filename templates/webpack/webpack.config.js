82799c074ea2009fe7d45753bbcbf9e60443addb
wmarchment <mon@westleymon.com>
2017-08-01 11:16:47 -0700

    updating config and dependencies to webpack 2

--- templates/webpack/webpack.config.js
+++ templates/webpack/webpack.config.js
@@ -8,23 +8,25 @@ var PROD = package.config.environment == 'dev' ? false : true;
 
 var modules = fs.readdirSync(package.config.themeAssetsDir + "/javascript/block/");
 modules.forEach(function (module) {
-    var scopedModule = 'block/' + module.replace(/\.js$/, '');
+    var scopedModule = './' + package.config.themeAssetsDir + '/javascript/block/' + module; // uses .js extension
+    var moduleName = 'block/' + module.replace(/\.js$/, '');
     if (scopedModule !== 'block/module' && scopedModule !== 'block/config') {
-        complexEntry[scopedModule] = scopedModule;
+        complexEntry[moduleName] = scopedModule;
     }
 });
 
 var layoutModules = fs.readdirSync(package.config.themeAssetsDir + "/javascript/layout/");
-layoutModules .forEach(function (module) {
-    var scopedModule = 'layout/' + module.replace(/\.js$/, '');
+layoutModules.forEach(function (module) {
+    var scopedModule = './' + package.config.themeAssetsDir + '/javascript/layout/' + module; // uses .js extension
+    var moduleName = 'layout/' + module.replace(/\.js$/, '');
     if (scopedModule !== 'layout/module' && scopedModule !== 'layout/config') {
-        complexEntry[scopedModule] = scopedModule;
+        complexEntry[moduleName] = scopedModule;
     }
 });
 
 var commonsModules = fs.readdirSync(package.config.themeAssetsDir + "/javascript/global/");
 commonsModules.forEach(function(module) {
-    var scopedModule = 'global/' + module; // uses .js extension
+    var scopedModule = './' + package.config.themeAssetsDir + '/javascript/global/' + module; // uses .js extension
     commons.push(scopedModule); // creates an array for
 
 });
@@ -34,7 +36,7 @@ complexEntry['commons'] = commons; // push commons as an entry point
 module.exports = {
     entry: complexEntry,
     output: {
-        path: package.config.themeAssetsDir + "/build",
+        path:  __dirname + '/' + package.config.themeAssetsDir + "/build",
         filename: PROD ? "[name].min.js" : "[name].built.js"
     },
     // devtool: "source-map",
@@ -42,45 +44,99 @@ module.exports = {
         new webpack.optimize.UglifyJsPlugin(),
         new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.min.js'}),
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
+            $: 'jquery',
+            jQuery: 'jquery'
         })
     ] : [
         new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.built.js'}),
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
+            $: 'jquery',
+            jQuery: 'jquery'
         })
     ],
     module: {
         rules: [
-            {test: /bootstrap\/affix/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/alert/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/button/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/carousel/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/collapse/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/dropdown/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/modal/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/popover/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/scrollspy/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/tab/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/tooltip/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\/transition/, use: 'imports-loader?jQuery=jquery'},
-            {test: /ekko-lightbox/, use: 'imports-loader?jQuery=jquery'},
-            {test: /bootstrap\-accessibility\-plugin\/src\/js\/functions/,
-                use: 'imports-loader?$=jquery!exports?uniqueId,removeMultiValAttributes,focusable,visible'},
-            {test: /bootstrap\-accessibility\-plugin\/src\/js\/(carousel|collapse|dropdown|modal|tab)/,
-                use: 'imports-loader?$=jquery&_BAPF=bootstrap-accessibility-plugin/src/js/functions&uniqueId=>_BAPF.uniqueId&removeMultiValAttributes=>_BAPF.removeMultiValAttributes&focusable=>_BAPF.focusable&visible=>_BAPF.visible'},
-            {test: /jquery\..*/, use: 'imports-loader?jQuery=jquery'},
-            {test: require.resolve('jquery'), use: 'expose-loader?jQuery!expose?$'}
+            {
+                test: /bootstrap\/affix/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/alert/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/button/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/carousel/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/collapse/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/dropdown/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/modal/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/popover/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/scrollspy/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/tab/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/tooltip/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /bootstrap\/transition/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /ekko-lightbox/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /jquery\..*/,
+                use: 'imports-loader?jQuery=jquery'
+            },
+            {
+                test: /google-infobox/,
+                use: 'imports-loader?$=jquery!exports?InfoBox'
+            },
+            {
+                test: require.resolve('jquery'),
+                use: [
+                    {
+                        loader: 'expose-loader',
+                        query: 'jQuery'
+                    },
+                    {
+                        loader: 'expose-loader',
+                        query: '$'
+                    }
+                ]
+            }
         ]
     },
     resolve: {
         alias: {
-            'jquery': 'jquery/dist/jquery',//from node_modules
+            'jquery': 'jquery/dist/jquery',
             'bootstrap': 'bootstrap-sass/assets/javascripts/bootstrap',
             'bootstrap-accessibility-plugin': 'bootstrap-accessibility-plugin/src/js'
         },
-        modulesDirectories: [package.config.themeAssetsDir + "/javascript", "node_modules"]
+        modules: [package.config.themeAssetsDir + "/javascript", "node_modules"]
     }
 };
